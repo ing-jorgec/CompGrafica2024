4c19b539f5f4685620f870fb337d67d2bacd01b1
Jorge Collazos <jorge.collazos@udenar.edu.co>
2024-10-25 11:48:52 -0500

    feat: add new component & route logintest

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,11 +5,14 @@ import { FormularioPlantilla1Component } from './formularios/formulario-plantill
 import { ButtonModule } from 'primeng/button';
 import { LoginComponent } from './components/login/login.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { LoginTestComponent } from './components/login-test/login-test.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FormularioPlantilla1Component, ButtonModule, LoginComponent, ReactiveFormsModule],
+  imports: [RouterOutlet, FormularioPlantilla1Component, 
+    ButtonModule, LoginComponent, ReactiveFormsModule, LoginTestComponent  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 import { HomeComponent } from './components/home/home.component';
+import { LoginTestComponent } from './components/login-test/login-test.component';
 
 export const routes: Routes = [
 {
@@ -24,6 +25,10 @@ export const routes: Routes = [
     path: 'home',
     component: HomeComponent
 
+},
+{
+ path:'logintest',
+ component: LoginTestComponent
 },
 {
 path: '', redirectTo: 'home', pathMatch: 'full'
